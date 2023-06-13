d54980e096bd9538fe4b2f9644f750e082adc16a
Miguel Bartolomeu <bartolomeu.miguel@gmail.com>
2023-06-13 09:21:11 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
-  get 'events_rsvp/index'
-  get 'events_rsvp/show'
-  get 'events_rsvp/new'
-  get 'events_rsvp/create'
-  get 'events_rsvp/edit'
-  get 'events_rsvp/update'
-  get 'events_rsvp/destroy'
-  get 'community_users/index'
-  get 'community_users/show'
-  get 'community_users/new'
-  get 'community_users/create'
-  get 'community_users/edit'
-  get 'community_users/update'
-  get 'community_users/destroy'
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :communities do
+    resources :join_requests, only: [:index, :update, :destroy]
+    resources :community_users, only: [:index, :create, :update, :destroy]
+    resources :events do
+      resources :events_rsvp, only: [:index, :create, :destroy]
+      resources :tickets, only: [:index, :show, :create]
+    end
+  end
+
+  root to: 'communities#index'
 end
