205fdcb84a0ad00b3be0cb9ab546c9e0a84e2803
Miguel Bartolomeu <bartolomeu.miguel@gmail.com>
2023-06-13 10:24:29 -0600

    mroutes2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_06_13_000022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,7 +84,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_000022) do
     t.index ["users_id"], name: "index_tickets_on_users_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -114,8 +112,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_000022) do
   add_foreign_key "community_rsvps", "users", column: "users_id"
   add_foreign_key "event_rsvps", "events", column: "events_id"
   add_foreign_key "event_rsvps", "users", column: "users_id"
-
   add_foreign_key "tickets", "events", column: "events_id"
   add_foreign_key "tickets", "users", column: "users_id"
-
 end
