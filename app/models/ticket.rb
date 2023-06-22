517e42f7c5693b891eabc3ae79bc20a17ccb6d86
Guillaume Soyer <soyerg@gmail.com>
2023-06-22 09:32:58 -0600

    done

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,9 @@ class Ticket < ApplicationRecord
     message: "%{value} is not a valid ticket model" }
   belongs_to :event
   has_many :user_tickets
+  has_many :users, through: :user_tickets
+
+  def ticket_counter(event)
+    vip_ticket = se
+  end
 end
