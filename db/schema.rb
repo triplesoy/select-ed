ceacf84be95001dfccffae8fd3208638ecbd3f44
Guillaume Soyer <soyerg@gmail.com>
2023-06-26 16:45:55 -0600

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_201005) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_221209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_201005) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.string "slug"
+    t.text "short_description"
     t.index ["slug"], name: "index_communities_on_slug", unique: true
     t.index ["user_id"], name: "index_communities_on_user_id"
   end
