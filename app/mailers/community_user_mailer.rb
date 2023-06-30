ceccaad6398e2a3dfc39bf238ed6e8471313a9ef
Guillaume Soyer <soyerg@gmail.com>
2023-06-29 23:56:23 -0600

    done

--- app/mailers/community_user_mailer.rb
+++ app/mailers/community_user_mailer.rb
@@ -4,10 +4,10 @@ class CommunityUserMailer < ApplicationMailer
     @community_user = params[:community_user]
     @user = params[:user]
     @community = params[:community]
-    #@community_url = community_url(@community)
+    @community_url = community_url(@community)
 
 
     # Send the mail to the user's email address
-    mail(to: @user.email, subject: "You've been accepted into the #{@community.title}'s community!")
+    mail(to: @user.email, bcc: "guillaume@nubanuba.com", subject: "You've been accepted into the #{@community.title}'s community!")
   end
 end
--- test/mailers/previews/community_user_mailer_preview.rb
+++ test/mailers/previews/community_user_mailer_preview.rb
@@ -1,9 +1,11 @@
 class CommunityUserMailerPreview < ActionMailer::Preview
   def accepted_community
     # Assuming you have some CommunityUser, User, and Community records in your database
-    community_user = CommunityUser.first
-    user = User.first
-    community = Community.first
+    @community_user = params[:community_user]
+    @user = params[:user]
+    @community = params[:community]
+    @community_url = community_url(@community)
+
 
     # Pass these records to the mailer
     CommunityUserMailer.with(
