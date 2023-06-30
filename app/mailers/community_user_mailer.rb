981d43428a9793a51f51823479122fb60000c322
Guillaume Soyer <soyerg@gmail.com>
2023-06-29 19:11:51 -0600

    done

--- app/mailers/community_user_mailer.rb
+++ app/mailers/community_user_mailer.rb
@@ -1,11 +1,10 @@
 class CommunityUserMailer < ApplicationMailer
-  def accepted_community
-    # The instance variables are set from the params that were passed in
-    @community_user = params[:community_user]
-    @user = params[:user]
-    @community = params[:community]
+  def accepted_community(community_user)
+    @community_user = community_user
+    @user = @community_user.user
+    @community = @community_user.community
+    @community_url = community_url(@community)
 
-    # Send the mail to the user's email address
     mail(to: @user.email, subject: "You've been accepted into the #{@community.title}'s community!")
   end
 end
