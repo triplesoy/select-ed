3c24e81ba7655ac797f1a90a43d44e90b3771147
Guillaume Soyer <soyerg@gmail.com>
2023-08-09 22:59:45 -0600

    Updated Sidekiq configuration for Heroku

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+require 'sidekiq/web'
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+end
--- test/mailers/previews/new_join_request_mailer_preview.rb
+++ test/mailers/previews/new_join_request_mailer_preview.rb
@@ -1,5 +1,5 @@
 # test/mailers/previews/new_join_request_mailer_preview.rb
-class NewJoinRequestMailerPreview < ActionMailer::Preview
+class NewJoinRequestMailerP < ActionMailer::Preview
   def new_join_request_notification
     join_request = CommunityJoinRequest.first
     NewJoinRequestMailer.new_join_request_notification(join_request)
